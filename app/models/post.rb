f68d8cbba6ff5f3c0359d11fa936f1e4c11f3891
あざらし <hide-0726@hotmail.co.jp>
2015-04-10 00:26:53 +0900

    remove いらないファイル

--- app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-# validates :category_id , presense: true
-# validates :abst    , presense: true
-# validates :title       , presense: true
-# validates :content     , presense: true
-end
