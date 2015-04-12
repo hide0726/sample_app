95736b548e3e8737715f44bb97503d689228cff8
あざらし <hide-0726@hotmail.co.jp>
2015-04-13 03:01:38 +0900

    ý5:q

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
    if @user.save
      sign_in @user
      flash[:success] = "Welcome to the Sample App!"
-     redirect_to @user
+     redirect_back_or @user
    else
     render 'new'
    end
